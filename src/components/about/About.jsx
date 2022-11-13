46f5ba9021e2fbe4b341813ae003e3052968a306
belayhun-arage <belayhun24arage@gmail.com>
2022-11-13 17:19:44 +0300

    first-commit

---
+++ src/components/about/About.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import './about.css'
+// import ME from '../../assets/fani.jpg'
+import ME from '../../assets/me.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
+
+const About = () => {
+    return (
+        <section id='about'>
+            <h5>Get To Know</h5>
+            <h2>About Me</h2>
+
+            <div className="container about__container">
+                <div className="about__me">
+                    <div className="about__me-img">
+                        <img src={ME} alt="About Me"></img>
+                    </div>
+                </div>
+
+                <div className="about__content">
+                    {/* <div className="about__cards">
+                        <article className='about__card'> 
+                            <FaAward  className='about__icon'/>
+                            <h5>Experience</h5>
+                            <small>5+ Years Working</small>
+                        </article>
+
+                        <article className='about__card'> 
+                            <FiUsers  className='about__icon'/>
+                            <h5>Clients</h5>
+                            <small>100+ Worldwide</small>
+                        </article>
+
+                        <article className='about__card'> 
+                            <VscFolderLibrary  className='about__icon'/>
+                            <h5>Projects</h5>
+                            <small>20+ Completed</small>
+                        </article>
+                    </div> */}
+
+                    <p>
+                        Hello :) Im Belayhun Arage, a front-end developer from Addis Ababa, Ethiopis.  <br/> 
+                        <br/>
+                        Well-qualified Full Stack Developer familiar with wide range of programming
+                    utilities and languages. Knowledgeable of backend and frontend development
+                    requirements. Handles any part of process with ease. Collaborative team player
+                    with excellent technical abilities and offering diverse customer projects.
+                    </p>
+
+                    <a href="#contact" className='btn btn-primary'> Let's Talk</a>
+                   
+                </div>
+            </div>
+
+
+        </section>
+    )
+}
+
+export default About
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,116 @@
+import React from 'react'
+import './services.css'
+import {BiCheck} from 'react-icons/bi'
+
+const Services = () => {
+    return (
+        <section id='services'>
+            <h5>Where I Have Worked</h5>
+            <h2>Experience</h2>
+
+            <div className="container services__container">
+            <article className="service">
+                    <div className="service__head">
+                    <h3>Ministry of Innovation and Technology MINT - Addis Ababa, Ethiopia</h3>
+                        <h3>Web Developer</h3>
+                        <h3>May 2021 - Sep 2021 </h3>
+                    </div>
+
+                    <ul className='service__list'>
+                        <li><BiCheck className='service__list-icon'/></li>
+                        <p>Built React JS project.</p>
+                    </ul>
+                    <ul className='service__list'>
+                        <li><BiCheck className='service__list-icon'/></li>
+                        <p>Designed user interface components for web apps</p>
+                    </ul>
+                    <ul className='service__list'>
+                        <li><BiCheck className='service__list-icon'/></li>
+                        <p>Worked on different frameworks in collaboration with team members</p>
+                    </ul>
+                    <ul className='service__list'>
+                        <li><BiCheck className='service__list-icon'/></li>
+                        <p>Deployed web app to cloud infrastructures</p>
+                    </ul>
+                    {}
+                </article>
+                <article className="service">
+                    <div className="service__head">
+                    <h3>Back To School Ethiopia</h3>
+                        <h3>Back-End Developer </h3>
+                        <h3>oCT 2021 - Dec 2021 </h3>
+                    </div>
+
+                    <ul className='service__list'>
+                        <li><BiCheck className='service__list-icon'/></li>
+                        <p>Built back-end for a website using NodeJS.</p>
+                    </ul>
+                    <ul className='service__list'>
+                        <li><BiCheck className='service__list-icon'/></li>
+                        <p>Designed database schema</p>
+                    </ul>
+                
+                    {}
+                </article>
+                <article className="service">
+                    <div className="service__head">
+                        <h3>Etech S.C</h3>
+                        <h3>Angular Developer</h3>
+                        <h3>Aug 2022 - Sep 2022 </h3>
+                    </div>
+
+                    <ul className='service__list'>
+                        <li><BiCheck className='service__list-icon'/></li>
+                        <p>Developed an angular web app</p>
+                    </ul>
+                    <ul className='service__list'>
+                        <li><BiCheck className='service__list-icon'/></li>
+                        <p>Designed user interface components for the web app.</p>
+                    </ul>
+                    <ul className='service__list'>
+                        <li><BiCheck className='service__list-icon'/></li>
+                        <p>Integrated with back end </p>
+                    </ul>
+                    {}
+                </article>
+                <article className="service">
+                    <div className="service__head">
+                    <h3>Santim Pay Financial Solution</h3>
+                        <h3>Front End Developer</h3>
+                        <h3>Sep 2022 - Present </h3>
+                    </div>
+                    <ul className='service__list'>
+                        <li><BiCheck className='service__list-icon'/></li>
+                        <p>Disigning UI for a website using VueJS</p>
+                    </ul>
+                    <ul className='service__list'>
+                        <li><BiCheck className='service__list-icon'/></li>
+                        <p>Disigning admin pannel for a website using VueJS</p>
+                    </ul>
+
+                    <ul className='service__list'>
+                        <li><BiCheck className='service__list-icon'/></li>
+                        <p>Integrating front-end components with back end api's UI for a website using VueJS</p>
+                    </ul>
+                </article>
+                <article className="service">
+                    <div className="service__head">
+                    <h3>Monster India</h3>
+                        <h3>Virtually assisting VueJS students</h3>
+                        <h3>Sep 2022 - Present </h3>
+                    </div>
+
+                    <ul className='service__list'>
+                        <li><BiCheck className='service__list-icon'/></li>
+                        <p>Assisting students on the road map for VueJS</p>
+                    </ul>
+                </article>
+                {/* END OF Web Development */}
+            </div>
+
+
+        </section>
+    )
+}
+
+export default Services
